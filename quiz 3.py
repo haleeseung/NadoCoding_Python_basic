717c1ddac925c1c1051f77e3547bf93fb535a094
haleeseung <127108173+haleeseung@users.noreply.github.com>
2023-08-14 00:02:08 +0900

    나도 코딩 유튜브 파이썬 수업
    유튜브 파이썬 수업을 듣고 퀴즈 내주신 문제 풀기.

---
+++ quiz 3.py
@@ -0,0 +1,24 @@
+# Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+
+# 예) http://naver.com
+# 규칙 1: http:// 부분은 제외 => naver.com
+# 규칙 2: 처음 만나는 점(.) 이후 부분은 제외 => naver
+# 규칙 3: 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+#                 (nav)           (5)         (1)         (1)
+# 예) 생성된 비밀번호 : nav51!
+
+# 나의 답안
+# site = "http://naver.com"
+# replace_http = site.replace("http://", "")
+# replace_http_dot = replace_http.replace(".com", "")
+# print(str(replace_http_dot[:3]) + str(len(replace_http_dot)) + str(replace_http_dot.count('e')) + "!")
+
+# 예시 답안
+# url = "http://naver.com"
+# url = "http://daum.net"
+# url = "http://youtube.com"
+# my_str = url.replace("http://", "") # 규칙 1
+# my_str = my_str[:my_str.index(".")] # 규칙 2
+# # my_str[0:5] -> 0 ~ 5 직전까지. (0, 1, 2, 3, 4)
+# password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+# print("{0}의 비밀번호는 {1} 입니다.".format(url, password))
